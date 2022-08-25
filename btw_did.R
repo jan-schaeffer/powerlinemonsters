65e540c50f106584156248c3650867344060a622
jan-schaeffer <jan.schaeffer@onmail.com>
2022-08-25 17:32:02 +0200

    Add CS Estimation in R

--- btw_did.R
+++ btw_did.R
@@ -6,35 +6,51 @@ setwd("/Users/jan/Dropbox/UP_EPQM/2222/MA/powerlinemonsters")
 #install.packages("ivreg")
 library(did)
 library(rio)
+library(ggplot2)
 
 # read in the data set
-data <-import("/Users/jan/Dropbox/UP_EPQM/2222/MA/powerlinemonsters/data/btw_treat.csv")
-summary(data)
+btw <-import("/Users/jan/Dropbox/UP_EPQM/2222/MA/powerlinemonsters/data/btw_treat.csv")
+summary(btw)
+
+erst <- subset(btw, first_vote == 1)
+zweit <- btw[btw$second_vote == 1,]
+summary(erst)
+
+parties <- c('Union', 'SPD', 'FDP', 'Linke', 'Grüne', 'Andere')
+treatments <-  c('treatment_30', 'treatment_60', 'treatment_100')
+
+for(party in parties){
+  for(treatment in treatments){
+    out <- att_gt(yname = party,
+                  gname = treatment,
+                  idname = "AGS",
+                  panel = TRUE,
+                  tname = "year",
+                  xformla = ~ C(state_id), # ~ pop_density + female + foreign + unemployed + avg_income + avg_age + catholic,
+                  data = erst,
+                  est_method = "reg",
+                  anticipation = 0,
+                  control_group = "nevertreated",
+                  clustervars = c("AGS", "state_id"),
+                  #bstrap = TRUE,
+                  #cband = TRUE,
+                  allow_unbalanced_panel = TRUE,
+    )
+    ggdid(out, ylim = c(floor(min(out$att - out$se * 2.345 - 1)), ceiling(max(out$att + out$se * 2.345 + 1))))
+    ggsave(sprintf("/Users/jan/Dropbox/UP_EPQM/2222/MA/powerlinemonsters/figures/R/%s_%s.png", party, treatment))
+    es <- aggte(out, type = "dynamic")
+    ggdid(es, ylim = c(floor(min(es$att.egt - es$se.egt * 2.345 - 1)), ceiling(max(es$att.egt + es$se.egt * 2.345 + 1))))
+    ggsave(sprintf("/Users/jan/Dropbox/UP_EPQM/2222/MA/powerlinemonsters/figures/R/ES_%s_%s.png", party, treatment))
+  }
+}
 
 
-out <- att_gt(yname = "Union",
-              gname = "treatment_30",
-              idname = "AGS",
-              panel = TRUE,
-              tname = "year",
-              xformla = ~1,
-              data = data,
-              est_method = "ipw",
-              anticipation = 0,
-              control_group = "notyettreated",
-              clustervars = c("AGS", "state_id"),
-              bstrap = TRUE,
-              cband = TRUE,
-              allow_unbalanced_panel = TRUE,
-)
 
-summary(out)
-
-ggdid(out, ylim = c(-1.5, 3.5))
-
-es <- aggte(out, type = "dynamic")
+es <- aggte(out, type = "dynamic", balance_e=1)
 summary(es)
 ggdid(es)
+ggdid(es, ylim = c(floor(min(es$att.egt - es$se.egt * 2.345 - 1)), ceiling(max(es$att.egt + es$se.egt * 2.345 + 1))))
+ggsave(sprintf("/Users/jan/Dropbox/UP_EPQM/2222/MA/powerlinemonsters/figures/R/ES_%s_%s.png", party, treatment))
 
 
 group_effects <- aggte(out, type = "group")
