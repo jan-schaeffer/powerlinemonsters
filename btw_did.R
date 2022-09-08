a6375b83c0d9ae60178d7c69ad0a9613e05cd019
jan-schaeffer <jan.schaeffer@onmail.com>
2022-09-08 12:12:30 +0200

    finalize pooled BTW nc figure

--- btw_did.R
+++ btw_did.R
@@ -1,52 +1,22 @@
 # Clear the R environment and set working directory
 rm(list = ls())
 setwd("/Users/jan/Dropbox/UP_EPQM/2222/MA/powerlinemonsters")
-
+path <- "/Users/jan/Dropbox/UP_EPQM/2222/MA/powerlinemonsters"
 # Install and load additional packages needed
-#install.packages("ivreg")
+#install.packages("patchwork")
 library(did)
 library(rio)
 library(ggplot2)
+library(patchwork)
+library(ggpubr)
 
 # read in the data set
 btw <-import("/Users/jan/Dropbox/UP_EPQM/2222/MA/powerlinemonsters/data/btw_control.csv")
 summary(btw)
 
 erst <- subset(btw, first_vote == 1)
-by <- subset(erst, Land == "BY")
-zweit <- btw[btw$second_vote == 1,]
-summary(erst)
 
-parties <- c('Union', 'SPD', 'FDP', 'Linke', 'Grüne', 'Andere')
-treatments <-  c('treatment_30', 'treatment_60', 'treatment_100')
-
-for(party in parties){
-  for(treatment in treatments){
-    out <- att_gt(yname = party,
-                  gname = treatment,
-                  idname = "AGS",
-                  panel = TRUE,
-                  tname = "year",
-                  xformla = ~ C(state_id), # ~ pop_density + female + foreign + unemployed + avg_income + avg_age + catholic,
-                  data = erst,
-                  est_method = "reg",
-                  anticipation = 0,
-                  control_group = "nevertreated",
-                  clustervars = c("AGS", "state_id"),
-                  #bstrap = TRUE,
-                  #cband = TRUE,
-                  allow_unbalanced_panel = TRUE,
-    )
-    ggdid(out, ylim = c(floor(min(out$att - out$se * 2.345 - 1)), ceiling(max(out$att + out$se * 2.345 + 1))))
-    ggsave(sprintf("/Users/jan/Dropbox/UP_EPQM/2222/MA/powerlinemonsters/figures/R/%s_%s.png", party, treatment))
-    es <- aggte(out, type = "dynamic")
-    ggdid(es, ylim = c(floor(min(es$att.egt - es$se.egt * 2.345 - 1)), ceiling(max(es$att.egt + es$se.egt * 2.345 + 1))))
-    ggsave(sprintf("/Users/jan/Dropbox/UP_EPQM/2222/MA/powerlinemonsters/figures/R/ES_%s_%s.png", party, treatment))
-  }
-}
-
-
-out <- att_gt(yname = 'Union',
+result <- att_gt(yname = 'Union',
               gname = 'treatment_15',
               idname = "AGS",
               panel = TRUE,
@@ -62,29 +32,103 @@ out <- att_gt(yname = 'Union',
               allow_unbalanced_panel = FALSE,
               #print_details = TRUE
 )
-summary(out)
-es <- aggte(out, type = "group", bstrap = TRUE, clustervars = c("AGS"))
-summary(es)
-ggdid(es, ylim = c(floor(min(es$att.egt - es$se.egt * 2.345 - 1)), ceiling(max(es$att.egt + es$se.egt * 2.345 + 1))))
+results[[length(results)+1]] <- result
+# calculate ATTs
+att <- aggte(result, type = "group", bstrap = TRUE, clustervars = c('AGS'))
+att <- c(att$att.egt)
+names(att) <- c('2005', '2010', '2014')
+att <- aggte(result, type = "group", bstrap = TRUE, clustervars = c('AGS'))
+se <- c(att$se.egt)
+names(se) <- c('2005', '2010', '2014')
 
-library(did)
-trace(gplot, edit=TRUE)
+sprintf('Group 2005 ATT(SE): %s (%s) \n Group 2010 ATT(SE): %s (%s) \n Group 2014 ATT(SE): %s (%s)', 
+        att[['2005']], se[['2005']], att[['2010']], se[['2010']], att[['2014']], se[['2014']])
+# plot results
+result_fig <- ggdid(result, 
+                    ylim = c(floor(min(result$att - result$se * 2.345 - 1)), ceiling(max(result$att + result$se * 2.345 + 1))),
+                    ncol = 3, ax_text_size = 8, grtitle='') +
+  labs(caption = sprintf('Overall ATT (SE): %.3f (%.3f) \n Wald-P: %.3f \n # Obs.: %s' ,
+                         att$overall.att, att$overall.se, result$Wpval, result$n)) + 
+theme(plot.caption = element_text(hjust=0.5, size=10),
+      plot.margin=unit(c(0,0,0,0),"cm"))
+result_fig
 
 
-group_effects <- aggte(out, type = "group")
-summary(group_effects)
+leg <- get_legend(result_fig)
+
+# Convert to a ggplot and print
+as_ggplot(leg)
+# Plot ATT
+att_fig <- ggdid(att, lab_size = 10, legend=F, x_lab='', ax_text_size = 5, ylab='', title="ATT") + 
+  labs(caption = sprintf('Group 2005 ATT(SE): %.3f (%.3f) \n Group 2010 ATT(SE): %.3f (%.3f) \n Group 2014 ATT(SE): %.3f (%.3f)', 
+                         att[['2005']], se[['2005']], att[["2010"]], se[['2010']], att[['2014']], se[['2014']])) + 
+  theme(plot.caption = element_text(hjust=0.5, size=10), axis.title.x = element_blank(),
+        plot.margin=unit(c(0,0,0,0),"cm"))
+
+combined_fig <- ggarrange(result_fig, att_fig, widths = c(3, 1)) + #, heights=c(1, 0.7)) +  
+  theme(plot.margin=unit(c(0,0,0,0),"cm"))
+combined_fig
+combined_fig_anno <- annotate_figure(combined_fig,
+                                top = text_grob(sprintf('%s', 'treatment_0')))
+figures[[length(figures)+1]] <- combined_fig_anno
+
+arranged_fig <- ggarrange(plotlist=figures, nrow = 1, ncol = 1, common.legend = TRUE, legend = 'bottom')
+arranged_fig
+final_fig <- annotate_figure(arranged_fig,
+                             top = text_grob(sprintf("Effect on %s's vote share", 'Union'), face = "bold", size = 14),
+                             bottom = text_grob('Control Group: Never Treated. Estimation Method: Regression \n SE clustered on the municipality level.', size = 10))
+final_fig
+
+cap =sprintf('Group 2005 ATT(SE): %.3f (%.3f) \n Group 2010 ATT(SE): %.3f (%.3f) \n Group 2014 ATT(SE): %.3f (%.3f)', 
+             as.numeric(att$att.egt[[1]]), as.numeric(att$se.egt[[1]]), as.numeric(att$att.egt[[2]]), as.numeric(att$att.se[[2]]), as.numeric(att$att.egt[[3]]), as.numeric(att$se.egt[[3]]))
+cap
+
+att <- aggte(result, type = "group", bstrap = TRUE, clustervars = c('AGS'))
+sprintf('%.3f _ %.3f', att$att.egt[[1]], att$se.egt[[1]])
 
 
-library(did)
-data(mpdta)
-View(mpdta)
-out <- att_gt(yname = "lemp",
-              gname = "first.treat",
-              idname = "countyreal",
-              tname = "year",
-              xformla = ~1,
-              data = mpdta,
-              est_method = "reg"
-)
+sprintf('%s', att[['2005']])
+sprintf('%s %s', att[['2005']], att[["2010"]])
 
-summary(out)
+
+att <- c(att$att.egt)
+names(att) <- c('2005', '2010', '2014')
+att <- aggte(result, type = "group", bstrap = TRUE, clustervars = c('AGS'))
+se <- c(att$se.egt)
+names(se) <- c('2005', '2010', '2014')
+
+do.call(sprintf, c(list("%s %s %s"), att$att.egt))
+
+att$att.egt
+
+stats = as.data.frame(att$att.egt)
+names(stats) <- c('a', 'b', 'c')
+stats$a
+att = as.numeric(stats[1, 1])
+att
+
+View(stats)
+
+cap =sprintf('Group 2005 ATT(SE): %.3f (%.3f)', as.numeric(stats[1, 1]), as.numeric(stats[1, 2]))
+cap
+                                                          
+att
+stats
+
+stat <-  as.numeric(att$att.egt[1])
+stat
+as_numer
+
+as.numeric(att$att.egt[1])
+
+.rs.restartR() 
+library(ggplot2)
+library(patchwork)
+library(ggpubr)
+
+library(ggplot2)
+
+# Very basic chart
+ggplot( mtcars , aes(x=mpg, y=wt)) + 
+  geom_point() + theme(axis.title.x = element_text(angle = 90, color="red", size=15, face=-100)) 
+
